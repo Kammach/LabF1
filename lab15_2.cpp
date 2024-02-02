f6174f4af8bb1773d713a4b05200abacd7dc5231
Kammach <153260807+Kammach@users.noreply.github.com>
2024-02-02 17:48:36 +0700

    Update lab15_2.cpp

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -6,8 +6,9 @@ int main(){
 	int data[] = {7,8,2,1,2,4,6,9,0,1}; 
 	cout << "Before: ";
 	for(int i = 0; i<10; i++) cout << data[i] << " ";
-	sort(&data[0],&data[10]);
+	sort(&data[0],&data[5]);
+	sort(&data[5],&data[10]);
 	cout << "\nAfter: ";
-	for(int i = 0; i<10; i++) cout << data[i] << " ";
+	for(int j = 0; j<10; j++) cout << data[j] << " ";
 	return 0;
 }
