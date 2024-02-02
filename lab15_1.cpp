bd52ae9354ae85e241f91e6b67e44633376e32f4
Kammach <153260807+Kammach@users.noreply.github.com>
2024-02-02 17:47:38 +0700

    Update lab15_1.cpp

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,67 @@
-include <iostream>
+#include <iostream>
 using namespace std;
+
+
 
 int main(){
 
-	
+    int a = 5;
+    char b = 'A';
+    char &c = b;
+    int *x = &a;
+    char *y = &b;
+    int **z= &x; 
+
+	cout << a << " ";
+	cout << b << " ";
+	cout << c << " ";
+	cout << x << " ";
+	cout << &y << " ";
+	cout << z << " " << endl;
+
+	cout << &a << " ";
+	cout << x << " ";
+	cout << x << " ";
+	cout << &x << " ";
+	cout << &y << " ";
+	cout << &z << " " << endl;
+
+	c = 'F';
+
+	cout << a << " ";
+	cout << b << " ";
+	cout << c << " ";
+	cout << x << " ";
+	cout << &y << " ";
+	cout << z << " " << endl;
+
+	*y = 'W';
+
+	cout << a << " ";
+	cout << b << " ";
+	cout << c << " ";
+	cout << x << " ";
+	cout << &y << " ";
+	cout << z << " " << endl;
+
+	*x = 6;
+
+	cout << a << " ";
+	cout << b << " ";
+	cout << c << " ";
+	cout << x << " ";
+	cout << &y << " ";
+	cout << z << " " << endl;
+
+	**z = 7;
+
+	cout << a << " ";
+	cout << b << " ";
+	cout << c << " ";
+	cout << x << " ";
+	cout << &y << " ";
+	cout << z << " " << endl;
+
 	
 	return 0;
 }
